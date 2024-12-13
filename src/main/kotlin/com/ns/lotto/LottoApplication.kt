6789295f841ca04d0b7d24abb7a293c9bef28e8c
namseokkim <skatjr30@gmail.com>
2024-12-14 08:58:58 +0900

    feat : 매주 토요일 20 ~ 21시 번호 업데이트

--- src/main/kotlin/com/ns/lotto/LottoApplication.kt
+++ src/main/kotlin/com/ns/lotto/LottoApplication.kt
@@ -2,7 +2,9 @@ package com.ns.lotto
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.scheduling.annotation.EnableScheduling
 
+@EnableScheduling
 @SpringBootApplication
 class LottoApplication
 
--- src/main/kotlin/com/ns/lotto/lottery/presentation/shceduler/LotteryScheduler.kt
+++ src/main/kotlin/com/ns/lotto/lottery/presentation/shceduler/LotteryScheduler.kt
@@ -1,31 +1,40 @@
 package com.ns.lotto.lottery.presentation.shceduler
 
+import com.fasterxml.jackson.databind.JsonNode
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.ns.lotto.lottery.business.domain.entity.Lottery
-import com.ns.lotto.lottery.business.domain.repository.LotteryRepository
+import com.ns.lotto.lottery.business.domain.service.LotteryService
 import kotlinx.serialization.json.Json
+import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Component
 import org.springframework.web.client.RestTemplate
 
 @Component
-class LotteryScheduler(private val lotteryRepository: LotteryRepository) {
-
-    //    @Scheduled()
+class LotteryScheduler(private val lotteryService: LotteryService
+) {
+//    @Scheduled(cron = "0 * 20-21 ? * SAT")
+    @Scheduled(cron = "0 0/10 20-21 ? * SAT")
     fun updateLottery() {
         val restTemplate = RestTemplate()
 
-        var 회차 = 1
-        for (i in 1..1145) {
-            회차 = i
-            val url = "https://www.dhlottery.co.kr/common.do?method=getLottoNumber&drwNo=" + 회차
-            val json = restTemplate.getForObject(url, String::class.java)
-            val lotteryDto = json?.let { Json.decodeFromString<LotteryApiDto>(it) }
+        val latest = lotteryService.getLatest()
+        val latestId = latest.id?.plus(1L)
 
-            lotteryDto?.let { Lottery(it) }?.let { lotteryRepository.save(it) }
-            // 로또 번호 저장
+        val url = "https://www.dhlottery.co.kr/common.do?method=getLottoNumber&drwNo=" + latestId
+        val json = restTemplate.getForObject(url, String::class.java)
+
+        val objectMapper = jacksonObjectMapper()
+        val root: JsonNode = objectMapper.readTree(json)
+        val returnValue = root["returnValue"]?.asText() // "fail" 값 가져오기
+
+        // 실패인 경우
+        if (returnValue == "fail") {
+            print("fail이야!!")
+            return
         }
 
-
-
+        val lotteryDto = json?.let { Json.decodeFromString<LotteryApiDto>(it) }
+        lotteryDto?.let { Lottery(it) }?.let { lotteryService.save(it) }
 
     }
 }
